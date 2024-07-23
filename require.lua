47cf2343eafa5f321da1dfd0f22a452798d421aa
existentz <158862479+existentzwastaken@users.noreply.github.com>
2024-07-24 00:15:46 +0200

    Require Script | require.lua

---
+++ require.lua
@@ -0,0 +1,16 @@
+--[[
+
+*== RoEvents ==*
+
+->@author existentza
+->@creation_date 21/07/24 12:45 PM
+->@version v0.1
+->@update_date 24/07/24 00:09 AM
+
+require asset id 18626279464 for the updated version!
+
+]]-- 
+
+local RoEvents = require(18626279464)
+
+RoEvents:ListenInit()
